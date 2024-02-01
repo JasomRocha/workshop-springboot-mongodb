736c11c0853e3c3b8229f69a4d63ca68bb33d1d8
Jasom Rocha <jasomrocha27@gmail.com>
2024-02-01 20:40:07 -0300

    Creating Post and User together

--- src/main/java/com/jasomrocha/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/jasomrocha/workshopmongo/repository/PostRepository.java
@@ -5,10 +5,10 @@ package com.jasomrocha.workshopmongo.repository;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
-import com.jasomrocha.workshopmongo.domain.User;
+import com.jasomrocha.workshopmongo.domain.Post;
 
 @Repository
-public interface UserRepository extends MongoRepository<User, String>{
+public interface PostRepository extends MongoRepository<Post, String>{
 
 
 }
